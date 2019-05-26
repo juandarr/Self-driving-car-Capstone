cce2ee800b6891680fead7515c61e7a20e4aea70
Juan David Rios <juandarr@gmail.com>
2019-05-25 23:32:14 -0500

    add: initial frozen graph classifier to be used in traffic light detection; feat: initial variables defined in TLClassifier class.

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,31 @@
 from styx_msgs.msg import TrafficLight
+import numpy as np
+import tensorflow as tf
+
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        ## Test model
+        path_to_graph = r'traffic_light.pb' 
+        self.path_to_labels = r'udacity_label_map.pbtxt'
+        self.num_classes = 4
+        self.graph = load_graph(path_to_graph)
+        
+    def load_graph(graph_file):
+        """Loads a frozen inference graph"""
+        graph = tf.Graph()
+        with graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(graph_file, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+        return graph
+
+
+    def load_image_into_numpy_array(image):
+        (im_width, im_height) = image.size
+        return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
